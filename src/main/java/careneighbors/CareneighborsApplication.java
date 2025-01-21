c0d51a3c52ced1f0275429866946bbaa81ab45d9
Robbie <robbie@email.com>
2025-01-21 20:34:57 +0900

    initial init

---
+++ src/main/java/careneighbors/CareneighborsApplication.java
@@ -0,0 +1,13 @@
+package careneighbors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareneighborsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareneighborsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/careneighbors/CareneighborsApplicationTests.java
@@ -0,0 +1,13 @@
+package careneighbors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareneighborsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
