2ac48464c3f73075eb94f397e5353f22e2ae9135
k800712 <minsung712@yahoo.co.kr>
2025-01-23 13:21:04 +0900

    관계 설정

---
+++ src/main/java/careneighbors/caregiver/CaregiverPatient.java
@@ -0,0 +1,18 @@
+package careneighbors.caregiver;
+
+import careneighbors.patient.Patient;
+import jakarta.persistence.*;
+
+@Entity
+public class CaregiverPatient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Patient patient;
+
+    @ManyToMany
+    private Caregiver caregiver;
+}
